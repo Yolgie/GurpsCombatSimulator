e005fe77f0619d61f61f20dd6f73f8cf9a1eb145
Matthias Holzinger <Matthias.Holzinger@cloudflight.io>
2021-03-21 17:00:29 +0100

    gradle init result

---
+++ app/src/test/kotlin/at/cnoize/gcs/app/AppTest.kt
@@ -0,0 +1,11 @@
+package at.cnoize.gcs.app
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class AppTest {
+    @Test fun testAppHasAGreeting() {
+        val classUnderTest = App()
+        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    }
+}
